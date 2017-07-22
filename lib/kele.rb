62707b154ef43fb152cc9f0c3b96309221fa8218
jagsarai <sarai.jagvir@gmail.com>
2017-07-22 15:13:29 -0700

    get and create messages work

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,6 +21,7 @@ class Kele
     raise "Invalid Email or Password. Try Again." if post_response.code == 404
 
     @auth_token = post_response["auth_token"]
+    @user = post_response["user"]["email"]
   end
 
   def get_me
@@ -49,6 +50,57 @@ class Kele
       })
 
     @mentor_availability = JSON.parse(get_mentor.body)
+  end
+
+  def get_messages(thread_page = nil)
+
+    if thread_page
+      get_message = self.class.get(
+        "/message_threads",
+        query:{
+          :page => thread_page
+        },
+        headers:{
+          :content_type => 'application/json',
+          :authorization => @auth_token,
+        })
+    else
+      get_message = self.class.get(
+        "/message_threads",
+        headers:{
+          :content_type => 'application/json',
+          :authorization => @auth_token
+        })
+    end
+
+    @messages = JSON.parse(get_message.body)
+
+    if @messages["items"] == []
+      print "There are no messages on this page, please try a previous page."
+    else
+      @messages
+    end
+
+  end
+
+
+  def create_message(sender=@user, recipient_id, token, subject, message)
+    create_message = self.class.post(
+      "/messages",
+      body: {
+        :sender => sender,
+        :recipient_id => recipient_id,
+        :subject => subject,
+        :token => token,
+        :stripped_text => message
+      },
+      headers:{
+        :content_type => 'application/json',
+        :authorization => @auth_token
+      })
+
+    p create_message
+    puts create_message.body
   end
 
 end
